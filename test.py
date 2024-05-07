27b886650e2c47e01f5a94eb2f7cf2d470f25ce0
Linda20236767 <142080595+Linda20236767@users.noreply.github.com>
2024-05-07 20:39:57 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,29 +1,13 @@
+"""
+# My first app
+Here's our first attempt at using data to create a table:
+"""
+
 import streamlit as st
-with st.echo():
-    def get_user_name():
-        return 'John'
+import pandas as pd
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
 
-with st.echo():
-    # Everything inside this block will be both printed to the screen
-    # and executed.
-
-    def get_punctuation():
-        return '!!!'
-
-    greeting = "Hi there, "
-    value = get_user_name()
-    punctuation = get_punctuation()
-
-    st.write(greeting, value, punctuation)
-
-# And now we're back to _not_ printing to the screen
-foo = 'bar'
-st.write('Done!')
-
-import time
-
-with st.empty():
-    for seconds in range(60):
-        st.write(f"⏳ {seconds} seconds have passed")
-        time.sleep(1)
-    st.write("✔️ 1 minute over!")
+df
